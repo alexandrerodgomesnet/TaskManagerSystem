ea0ec3dcc82c605192a0e574fae1d598401a1fcc
Alexandre R. Gomes <alexandrerodgomes@hotmail.com>
2025-11-13 08:10:13 -0300

    fix: Ajustes na configuração de serviços.

--- src/TaskManagerSystem.Api/Configurations/ServicesConfigurations.cs
+++ src/TaskManagerSystem.Api/Configurations/ServicesConfigurations.cs
@@ -1,12 +1,13 @@
 using FluentValidation;
+using TaskManagerSystem.Application;
 using TaskManagerSystem.Application.Features.TaskItems;
-using TaskManagerSystem.Application.Features.Users;
 using TaskManagerSystem.Application.Interfaces.Repositories;
 using TaskManagerSystem.Application.Interfaces.Repositories.Base;
 using TaskManagerSystem.Application.Interfaces.Services;
-using TaskManagerSystem.Core;
 using TaskManagerSystem.Infrastructure.Repositories;
 using TaskManagerSystem.Infrastructure.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+using TaskManagerSystem.Infrastructure.Persistence.Context;
 
 namespace TaskManagerSystem.Api.Configurations;
 
@@ -14,12 +15,16 @@ public static class ServicesConfigurations
 {
     public static void ConfigureServices(this WebApplicationBuilder builder)
     {
+        builder.Services.AddDbContext<TaskManagerContext>(opt =>
+            opt.UseInMemoryDatabase("TaskManagerDb"));
+
         builder.Services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
         builder.Services.AddScoped<ITaskItemRepository, TaskItemRepository>();
-        builder.Services.AddScoped<IUserRepository, UserRepository>();
         builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
         builder.Services.AddScoped<ITaskItemService, TaskItemService>();
-        builder.Services.AddScoped<IUserService, UserService>();
+
+        builder.Services.AddAutoMapperFromAssemblies();
+        builder.Services.AddFromAssembliesValidators();
 
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddOpenApiDocument(config =>
@@ -28,20 +33,17 @@ public static class ServicesConfigurations
             config.Title = "TaskManagerSystemAPI v1";
             config.Version = "v1";
         });
-
-        builder.Services.AddValidatorsFromAssemblies([
-            typeof(Program).Assembly, 
-            typeof(CoreLayer).Assembly
-        ], includeInternalTypes: true);
-
-        // builder.Services.AddFluentValidationAutoValidation()
-        //     .AddValidatorsFromAssembly();
-                //.AddValidatorsFromAssemblyContaining<UserValidator>();
-
-        // builder.Services.AddSwaggerGen(c =>
-        // {
-        //     c.SwaggerDoc("v1",
-        //         new() { Title = "Tarefa Api", Version = "v1" });
-        // });
     }
+
+    private static IServiceCollection AddFromAssembliesValidators(this IServiceCollection services) =>
+            services.AddValidatorsFromAssemblies([
+                typeof(Program).Assembly, 
+                typeof(ApplicationLayer).Assembly
+            ], includeInternalTypes: true);
+
+    private static IServiceCollection AddAutoMapperFromAssemblies(this IServiceCollection services) =>
+        services.AddAutoMapper(
+            typeof(Program).Assembly, 
+            typeof(ApplicationLayer).Assembly
+        );
 }
